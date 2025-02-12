653d480d5996846ad18a528c1792fa78e4a8710d
karix <eivanov200606@mail.ru>
2025-02-12 22:08:03 +0700

    work tests

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -1,13 +1,50 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Collect\Collect;
 
 class CollectTest extends TestCase
 {
+    public function testOnly()
+    {
+        $collect = new Collect([
+            'name' => 'Alice',
+            'age' => 25,
+            'email' => 'alice@example.com',
+            'country' => 'USA'
+        ]);
+
+        $result = $collect->only(['name', 'email']);
+
+        $this->assertSame(['name' => 'Alice', 'email' => 'alice@example.com'], $result);
+    }
+
+
+    public function testFirst()
+    {
+        $collect = new Collect([1, 2, 3, 4, 5]);
+
+        $result = $collect->first();
+
+        $this->assertSame(1, $result);
+    }
+
     public function testCount()
     {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        $collect = new Collect([1, 2, 3, 4, 5]);
+
+        $result = $collect->count();
+
+        $this->assertSame(5, $result);
+    }
+
+    public function testToArray()
+    {
+        $collect = new Collect([1, 2, 3, 4, 5]);
+
+        $result = $collect->toArray();
+
+        $this->assertSame([1,2,3,4,5], $result);
     }
 
 }
